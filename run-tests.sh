cab0fe0b19505629eeaf46ebe310da9357b097f1
Chris Penner <christopher.penner@gmail.com>
2024-10-22 12:20:48 -0700

    Use custom ucm version

--- run-tests.sh
+++ run-tests.sh
@@ -2,18 +2,18 @@
 
 set -euxo pipefail
 
-release_file_name() {
-  case "$UCM_RELEASE" in
-    "release%2F0.5.25" | "release%2F0.5.26" | "release%2F0.5.27")
-      echo "ucm-linux.tar.gz"
-      ;;
-    *)
-      echo "ucm-linux-x64.tar.gz"
-      ;;
-  esac
-}
+# release_file_name() {
+#   case "$UCM_RELEASE" in
+#     "release%2F0.5.25" | "release%2F0.5.26" | "release%2F0.5.27")
+#       echo "ucm-linux.tar.gz"
+#       ;;
+#     *)
+#       echo "ucm-linux-x64.tar.gz"
+#       ;;
+#   esac
+# }
 
-curl --fail-with-body -L "https://github.com/unisonweb/unison/releases/download/${UCM_RELEASE}/$(release_file_name)" | tar -xz
+curl --fail-with-body -L "https://github.com/unisonweb/unison/actions/runs/11465576123/artifacts/2089481087" | tar -xz
 
 envsubst < cloud-tests.tpl.md > cloud-tests.md
 
