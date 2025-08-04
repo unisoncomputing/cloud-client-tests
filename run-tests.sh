9f90476fbe9cc4aa5ca2466cb7fbaa1f1778cf25
Cody Allen <cody.allen@unison.cloud>
2025-08-04 09:24:11 -0400

    Handle trunk ucm failing on in-place transcripts

--- run-tests.sh
+++ run-tests.sh
@@ -7,7 +7,18 @@ envsubst < cloud-tests.tpl.md > cloud-tests.md
 exit_status=0
 codebase="$PWD/unison-code"
 
-ucm -C "$codebase" transcript.fork --save-codebase-to "$codebase" cloud-tests.md || exit_status=$?
+run_transcript() {
+  case "$UCM_RELEASE" in
+    "release%2F0.5.25" | "release%2F0.5.41")
+      ucm -C "$codebase" transcript.fork --save-codebase-to "$codebase" cloud-tests.md
+      ;;
+    *)
+      ucm -C "$codebase" transcript.in-place cloud-tests.md
+      ;;
+  esac
+}
+
+run_transcript || exit_status=$?
 cat cloud-tests.output.md
 
 exit "$exit_status"
